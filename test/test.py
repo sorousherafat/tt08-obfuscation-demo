9e1ae22259bc5ee7926acffe069d87f1f6d11c63
Soroush Sherafat <sorousherafat@gmail.com>
2024-08-05 13:59:41 +0200

    updated project

--- test/test.py
+++ test/test.py
@@ -38,3 +38,10 @@ async def test_project(dut):
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+
+    dut.ui_in.value = 31
+    dut.uio_in.value = 21
+
+    await ClockCycles(dut.clk, 1)
+
+    assert dut.uo_out.value == 52
